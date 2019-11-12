971903edada24296b6c0026d47c80647c5434083
Paul J Shaw <pavlvsxavier@gmail.com>
2019-11-12 16:29:40 +0000

    Added registration form to register.php

--- register.php
+++ register.php
@@ -18,9 +18,41 @@
         	</p>
         	<p>
         		<label for="loginPassword">Password</label>
-        		<input type="password" name="loginPassword" id="loginPassword" value="" required>
+        		<input type="password" name="loginPassword" id="loginPassword" value="" placeholder="Your password" required>
         	</p>
 			<button type="submit" name="loginButton">LOG IN</button>
+        </form>
+        <form action="register.php" method="POST" id="registerForm">
+        	<h2>Create your free account</h2>
+        	<p>
+        		<label for="username">Username</label>
+        		<input type="text" name="username" id="username" value="" placeholder="e.g. bartSimpson" required>
+        	</p>
+        	<p>
+        		<label for="firstName">First name</label>
+        		<input type="text" name="firstName" id="firstName" value="" placeholder="e.g. Bart" required>
+        	</p>
+        	<p>
+        		<label for="lastName">Last name</label>
+        		<input type="text" name="lastName" id="lastName" value="" placeholder="e.g. Simpson" required>
+        	</p>
+        	<p>
+        		<label for="email1">Email</label>
+        		<input type="email" name="email1" id="email1" value="" placeholder="e.g. bart@simpson.com" required>
+        	</p>
+        	<p>
+        		<label for="email2">Confirm Email</label>
+        		<input type="email" name="email2" id="email2" value="" placeholder="e.g. bart@simpson.com" required>
+        	</p>
+        	<p>
+        		<label for="password1">Password</label>
+        		<input type="password" name="password1" id="password1" value="" placeholder="Your password" required>
+        	</p>
+        	<p>
+        		<label for="password2">Confirm Password</label>
+        		<input type="password" name="password2" id="password2" value="" placeholder="Your password" required>
+        	</p>
+			<button type="submit" name="registerButton">Sign Up</button>
         </form>
     </div>
 </body>
