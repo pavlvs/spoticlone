5bc000790edbfc9cbdd6e5e498ddb5b2366acbad
Paul J Shaw <pavlvsxavier@gmail.com>
2019-11-12 18:57:53 +0000

    set up validation functions for form fields

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -16,6 +16,22 @@ function sanitizeFormPassword($inputText) {
 	$inputText = strip_tags($inputText);
 	$inputText = str_replace(" ", "", $inputText);
 }
+
+function validateUsername($un) {
+	# code...
+}
+function validateFirstName($fn) {
+	# code...
+}
+function validateLastName($ln) {
+	# code...
+}
+function validateEmails($em1, $em2) {
+	# code...
+}
+function validatePasswords($pw1, $pw2) {
+	# code...
+}
 if (isset($_POST['registerButton'])) {
 	// register button was pressed
 	$username = sanitizeFormUsername($_POST['username']);
@@ -25,5 +41,11 @@ if (isset($_POST['registerButton'])) {
 	$email2 = sanitizeFormString($_POST['email2']);
 	$password1 = sanitizeFormPassword($_POST['password1']);
 	$password2 = sanitizeFormPassword($_POST['password2']);
+
+	validateUsername($username);
+	validateFirstName($firstName);
+	validateLastName($lastName);
+	validateEmails($email1, $email2);
+	validatePasswords($password1, $password2);
 }
 ?>
