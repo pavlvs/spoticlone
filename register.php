b5af5144b3b2b1babb32059b31bdc026dc51ef03
Paul J Shaw <pavlvsxavier@gmail.com>
2019-11-12 18:02:48 +0000

    moved form data sanitizing functions to register-handlers.php

--- register.php
+++ register.php
@@ -1,36 +1,3 @@
-<?php
-
-function sanitizeFormUsername($inputText) {
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	return $inputText;
-}
-function sanitizeFormString($inputText) {
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	$inputText = ucfirst(strtolower($inputText));
-	return $inputText;
-}
-
-function sanitizeFormPassword($inputText) {
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-}
-if (isset($_POST['loginButton'])) {
-	// Login button was pressed
-
-}
-if (isset($_POST['registerButton'])) {
-	// register button was pressed
-	$username = sanitizeFormUsername($_POST['username']);
-	$firstName = sanitizeFormString($_POST['firstName']);
-	$lastName = sanitizeFormString($_POST['lastName']);
-	$email1 = sanitizeFormString($_POST['email1']);
-	$email2 = sanitizeFormString($_POST['email2']);
-	$password1 = sanitizeFormPassword($_POST['password1']);
-	$password2 = sanitizeFormPassword($_POST['password2']);
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
