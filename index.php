caaff096743ea7996eadc7e8d3d7a078de59234b
Paul J Shaw <pavlvsxavier@gmail.com>
2019-11-18 21:44:08 +0000

    added music files and restructured index.php with included files

--- index.php
+++ index.php
@@ -20,81 +20,9 @@ if (isset($_SESSION['userLoggedIn'])) {
 <body>
 	<div id="mainContainer">
 		<div id="topContainer">
-			<div id="navBarContainer">
-				<nav class="navBar">
-					<a href="index.php" class="logo">
-						<img src="assets/images/icons/logo.png" alt="logo">
-					</a>
-					<div class="group">
-						<div class="navItem">
-							<a href="search.php" class="navItemLink">Search <img src="assets/images/icons/search.png" alt="search" class="icon"></a>
-						</div>
-					</div>
-					<div class="group">
-						<div class="navItem">
-							<a href="browse.php" class="navItemLink">Browse</a>
-						</div>
-						<div class="navItem">
-							<a href="your_music.php" class="navItemLink">Your Music</a>
-						</div>
-						<div class="navItem">
-							<a href="profile.php" class="navItemLink">Paul Shaw</a>
-						</div>
-
-					</div>
-				</nav>
-			</div><!--End of navBarContainer -->
+			<?php include "includes/navBarContainer.php"?>
 		</div><!--End of topContainer -->
-		<div id="nowPlayingBarContainer">
-			<div id="nowPlayingBar">
-				<div id="nowPlayingLeft">
-					<div class="content">
-						<span class="albumLink">
-							<img src="assets/images/square.jpg" alt="square" class="albumArtwork">
-						</span>
-						<div class="trackInfo">
-							<span class="trackName">
-								Happy Foo
-							</span>
-							<span class="artistName">
-								Pavlvs X
-							</span>
-						</div>
-					</div>
-				</div><!--End of nowPlayingLeft -->
-				<div id="nowPlayingCenter">
-					<div class="content playerControls">
-						<div class="buttons">
-							<button class="controlButton shuffle" title="Shuffle button"><img src="assets/images/icons/shuffle.png" alt="shuffle button"></button>
-							<button class="controlButton previous" title="previous button"><img src="assets/images/icons/previous.png" alt="previous button"></button>
-							<button class="controlButton play" title="play button"><img src="assets/images/icons/play.png" alt="play button"></button>
-							<button class="controlButton pause" title="pause button" style="display:none"><img src="assets/images/icons/pause.png" alt="pause button"></button>
-							<button class="controlButton next" title="next button"><img src="assets/images/icons/next.png" alt="next button"></button>
-							<button class="controlButton repeat" title="repeat button"><img src="assets/images/icons/repeat.png" alt="repeat button"></button>
-						</div>
-						<div class="playbackBar">
-							<span class="progressTime current">0:00</span>
-							<div class="progressBar">
-								<div class="progressBarBg">
-									<div class="progress"></div>
-								</div>
-							</div>
-							<span class="progressTime remaining">0:00</span>
-						</div>
-					</div>
-				</div><!--End of nowPlayingCenter -->
-				<div id="nowPlayingRight">
-					<div class="volumeBar">
-						<button class="controlButton volume" title="volume button"><img src="assets/images/icons/volume.png" alt="volume button"></button>
-						<div class="progressBar">
-							<div class="progressBarBg">
-								<div class="progress"></div>
-							</div>
-						</div>
-					</div>
-				</div><!--End of nowPlayingRight -->
-			</div><!--End of nowPlayingBar -->
-		</div><!--End of nowPlayingContainer -->
+		<?php include "includes/nowPlayingBarContainer.php"?>
 	</div><!--End of mainContainer -->
 </body>
 
