31d1fae1bb764da7d13419c00a53e758907997eb
Paul J Shaw <pavlvsxavier@gmail.com>
2019-11-18 20:16:06 +0000

    added and styled navBar groups and navBarItem

--- index.php
+++ index.php
@@ -25,6 +25,23 @@ if (isset($_SESSION['userLoggedIn'])) {
 					<a href="index.php" class="logo">
 						<img src="assets/images/icons/logo.png" alt="logo">
 					</a>
+					<div class="group">
+						<div class="navItem">
+							<a href="search.php" class="navItemLink">Search</a>
+						</div>
+					</div>
+					<div class="group">
+						<div class="navItem">
+							<a href="browse.php" class="navItemLink">Browse</a>
+						</div>
+						<div class="navItem">
+							<a href="your_music.php" class="navItemLink">Your Music</a>
+						</div>
+						<div class="navItem">
+							<a href="profile.php" class="navItemLink">Paul Shaw</a>
+						</div>
+
+					</div>
 				</nav>
 			</div><!--End of navBarContainer -->
 		</div><!--End of topContainer -->
