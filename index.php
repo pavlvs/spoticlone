70157bcb85ec0aa120bb99a18af8bafffb7ed41b
Paul J Shaw <pavlvsxavier@gmail.com>
2019-11-18 10:57:18 +0000

    added and styled control buttons

--- index.php
+++ index.php
@@ -21,7 +21,18 @@ if (isset($_SESSION['userLoggedIn'])) {
 	<div id="nowPlayingBarContainer">
 		<div id="nowPlayingBar">
 			<div id="nowPlayingLeft"></div>
-			<div id="nowPlayingCenter"></div>
+			<div id="nowPlayingCenter">
+				<div class="content playerControls">
+					<div class="buttons">
+						<button class="controlButton shuffle" title="Shuffle button"><img src="assets/images/icons/shuffle.png" alt="shuffle button"></button>
+						<button class="controlButton previous" title="previous button"><img src="assets/images/icons/previous.png" alt="previous button"></button>
+						<button class="controlButton play" title="play button"><img src="assets/images/icons/play.png" alt="play button"></button>
+						<button class="controlButton pause" title="pause button" style="display:none"><img src="assets/images/icons/pause.png" alt="pause button"></button>
+						<button class="controlButton next" title="next button"><img src="assets/images/icons/next.png" alt="next button"></button>
+						<button class="controlButton repeat" title="repeat button"><img src="assets/images/icons/repeat.png" alt="repeat button"></button>
+					</div>
+				</div>
+			</div>
 			<div id="nowPlayingRight"></div>
 		</div>
 	</div>
