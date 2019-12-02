bcc03e725ce7ed5a0701af6dc52adf29f29556b4
Paul J Shaw <pavlvsxavier@gmail.com>
2019-12-02 18:13:57 +0000

    Added functionality to control volume based on offsetX percentage of volumeBar width

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -22,11 +22,30 @@ $jsonArray = json_encode($resultArray);
                 //set time of song depending on position of mouse
                 timeFromOffset(e, this);
             }
-
         })
 
         $(".playbackBar .progressBar").mouseup(function(e){
             timeFromOffset(e, this);
+        })
+
+        $(".volumeBar .progressBar").mousedown(function(){
+            mouseDown = true;
+        })
+
+        $(".volumeBar .progressBar").mousemove(function(e){
+            if (mouseDown == true) {
+                var percentage = e.offsetX / $(this).width();
+                if (percentage >=0 && percentage <= 1) {
+                    audioElement.audio.volume = percentage;
+                }
+            }
+        })
+
+        $(".volumeBar .progressBar").mouseup(function(e){
+            var percentage = e.offsetX / $(this).width();
+            if (percentage >=0 && percentage <= 1) {
+                audioElement.audio.volume = percentage;
+            }
         })
 
         $(document).mouseup(function(){
