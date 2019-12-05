92b5a8224de7d3cf1a293c02d712b85f297d590c
Paul J Shaw <pavlvsxavier@gmail.com>
2019-12-05 11:00:40 +0000

    Revert "Implemented shuffling of songs with setShuffle() function shuffle array function and checking of shuffle variable status"
    This reverts commit 5fc0934611d0c63e3c951ab645618e9091166f34.

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -9,9 +9,9 @@ $jsonArray = json_encode($resultArray);
 ?>
 <script>
     $(function() {
-        var newPlaylist = <?php echo $jsonArray; ?>;
+        currentPlaylist = <?php echo $jsonArray; ?>;
         audioElement = new Audio();
-        setTrack(newPlaylist[0], newPlaylist, false);
+        setTrack(currentPlaylist[0], currentPlaylist, false);
         updateVolumeProgressBar(audioElement.audio);
 
         $("#nowPlayingBarContainer").on("mousedown touchstart mousemove touchmove", function(e) {
@@ -87,7 +87,7 @@ $jsonArray = json_encode($resultArray);
             currentIndex++;
         }
 
-        var trackToPlay = shuffle ? shuffledPlaylist[currentIndex] : currentPlaylist[currentIndex];
+        var trackToPlay = currentPlaylist[currentIndex];
         setTrack(trackToPlay, currentPlaylist, true);
     }
 
@@ -103,73 +103,32 @@ $jsonArray = json_encode($resultArray);
         $('.controlButton.volume img').attr('src', 'assets/images/icons/' + imageName);
     }
 
-    function setShuffle() {
-        shuffle = !shuffle;
-        var imageName = shuffle ? "shuffle-active.png" : "shuffle.png";
-        $('.controlButton.shuffle img').attr('src', 'assets/images/icons/' + imageName);
-
-        if (shuffle) {
-            // randomize playlist
-            shuffleArray(shuffledPlaylist);
-            currentIndex = shuffledPlaylist.indexOf(audioElement.currentlyPlaying.id);
-        }else {
-            // shuffle has been deactivated
-            // go back to regular playlist
-            currentIndex = currentPlaylist.indexOf(audioElement.currentlyPlaying.id);
-        }
-    }
-
-    function shuffleArray(a) {
-        var j, x, i;
-        for (var i = a.length; i; i--) {
-            j = Math.floor(Math.random() * i);
-            x = a[i - 1];
-            a[i - 1] = a[j];
-            a[j] = x;
-        }
-    }
-
     function setTrack(trackId, newPlaylist, play) {
-
-        if (newPlaylist != currentPlaylist) {
-            currentPlaylist = newPlaylist;
-            shuffledPlaylist = currentPlaylist.slice();
-            shuffleArray(shuffledPlaylist);
-        }
-
-        if (shuffle) {
-            currentIndex = shuffledPlaylist.indexOf(trackId);
-        }else {
-            currentIndex = currentPlaylist.indexOf(trackId);
-        }
-
+        currentIndex = currentPlaylist.indexOf(trackId);
         pauseSong();
 
         $.post("includes/handlers/ajax/getSongJson.php", {
             songId: trackId
         }, function(data) {
 
+
             var track = JSON.parse(data);
             $(".trackName").text(track.title);
-
             $.post("includes/handlers/ajax/getArtistJson.php", {
                 artistId: track.artist
             }, function(data) {
                 var artist = JSON.parse(data);
                 $(".artistName").text(artist.name);
             });
-
             $.post("includes/handlers/ajax/getAlbumJson.php", {
                 albumId: track.album
             }, function(data) {
                 var album = JSON.parse(data);
                 $("img.albumArtwork").attr("src", album.artworkPath);
             });
-
             audioElement.setTrack(track);
             playSong();
         });
-
         if (play) {
             // audioElement.play();
         }
@@ -209,7 +168,7 @@ $jsonArray = json_encode($resultArray);
         <div id="nowPlayingCenter">
             <div class="content playerControls">
                 <div class="buttons">
-                    <button class="controlButton shuffle" title="Shuffle button"><img src="assets/images/icons/shuffle.png" alt="shuffle button" onclick="setShuffle()"></button>
+                    <button class="controlButton shuffle" title="Shuffle button"><img src="assets/images/icons/shuffle.png" alt="shuffle button"></button>
                     <button class="controlButton previous" title="previous button"><img src="assets/images/icons/previous.png" alt="previous button" onclick="prevSong()"></button>
                     <button class="controlButton play" title="play button"><img src="assets/images/icons/play.png" alt="play button" onclick="playSong()"></button>
                     <button class="controlButton pause" title="pause button" style="display:none"><img src="assets/images/icons/pause.png" alt="pause button" onclick="pauseSong()"></button>
