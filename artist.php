c5aef8bbe433344bbf0a90071f3347f59aae7e43
Paul J Shaw <pavlvsxavier@gmail.com>
2019-12-07 17:07:03 +0000

    added album section to artist page

--- artist.php
+++ artist.php
@@ -24,6 +24,7 @@ $artist = new Artist($con, $artistId);
 </div>
 
 <div class="tracklistContainer borderBottom">
+    <h2>SONGS</h2>
     <ul class="tracklist">
         <?php
             $songIdArray = $artist->getSongIds();
@@ -80,4 +81,24 @@ EOT;
     tempPlaylist = JSON.parse(tempSongIds);
 </script>
     </ul>
+</div>
+<div class="gridViewContainer">
+    <h2>ALBUMS</h2>
+    <?php
+$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE artist='$artistId'");
+
+while ($row = mysqli_fetch_array($albumQuery)) {
+    echo "<div class='gridViewItem'>
+        <span role='link' tabindex='0' onclick='openPage(\"album.php?id="
+        . $row['id'] .
+        "\")'>
+            <img src='"
+        . $row['artworkPath'] .
+        "'><div class='gridViewInfo'>"
+        . $row['title'] .
+        "</div>
+        </span>
+        </div>";
+}
+?>
 </div>
