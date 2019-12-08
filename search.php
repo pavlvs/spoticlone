6d055cefc692e45549fbb5b738bb390a48c014c0
Paul J Shaw <pavlvsxavier@gmail.com>
2019-12-08 19:35:17 +0000

    implemented song search with LIKE $term and while fetch array

--- search.php
+++ search.php
@@ -28,3 +28,68 @@ if(isset($_GET['term'])){
         })
     })
 </script>
+
+
+<div class="tracklistContainer borderBottom">
+    <h2>SONGS</h2>
+    <ul class="tracklist">
+        <?php
+            $songsQuery = mysqli_query($con, "SELECT id FROM songs WHERE title LIKE '$term%'");
+            if (mysqli_num_rows($songsQuery) == 0) {
+                echo "<span class='noResults'>No songs found matching " . $term ."</span>";
+            }
+            $songIdArray = array();
+
+        $i = 1;
+    while ($row = mysqli_fetch_array($songsQuery)) {
+
+            if ($i > 15) {
+                break;
+            }
+            array_push($songIdArray, $row['id']);
+            $albumSong = new Song($con, $row['id']);
+            $albumArtist = $albumSong->getArtist();
+
+            echo <<<EOT
+            <li class="tracklistRow">
+            <div class="trackCount">
+            <img class="play" src="assets/images/icons/play-white.png" onclick="setTrack('
+EOT;
+            echo $albumSong->getId();
+            echo <<<EOT
+            ', tempPlaylist, true)">
+            <span class="trackNumber">$i</span>
+            </div>
+            <div class="trackInfo">
+                <span class="trackName">
+EOT;
+            echo $albumSong->getTitle();
+            echo <<<EOT
+                </span>
+                <span class="trackArtist">
+EOT;
+            echo $albumArtist->getName();
+            echo <<<EOT
+                </span>
+            </div>
+            <div class="trackOptions">
+                <img class="optionsButton" src="assets/images/icons/more.png">
+            </div>
+            <div class="trackDuration">
+                <span class="duration">
+EOT;
+            echo $albumSong->getDuration();
+            echo <<<EOT
+                </span>
+            </div>
+        <   /li>
+EOT;
+        $i++;
+        }
+        ?>
+<script >
+    var tempSongIds = '<?php echo json_encode($songIdArray);?>';
+    tempPlaylist = JSON.parse(tempSongIds);
+</script>
+    </ul>
+</div>
