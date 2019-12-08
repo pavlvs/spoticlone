3970a6050de856e5718a0664c654bf99b6071d65
Paul J Shaw <pavlvsxavier@gmail.com>
2019-12-08 17:11:14 +0000

    added script to search.php to capture input on keyup with timer and . val()

--- search.php
+++ search.php
@@ -10,5 +10,21 @@ if(isset($_GET['term'])){
 
 <div class="searchContainer">
     <h4>Search for an album, artist or song</h4>
-    <input type="text" name="" value="<?php echo $term; ?>" placeholder="Start typing..." class="searchInput">
+    <input type="text" name="" value="<?php echo $term; ?>" placeholder="Start typing..." class="searchInput" onfocus="this.selectionStart = this.selectionEnd = this.value.length;">
 </div>
+
+<script type="text/javascript">
+    $('.searchInput').focus();
+    $(function(){
+        var timer;
+
+        $('.searchInput').keyup(function () {
+            clearTimeout(timer);
+
+            timer = setTimeout(function () {
+                var val = $('.searchInput').val();
+                openPage("search.php?term=" + val);
+            }, 2000);
+        })
+    })
+</script>
