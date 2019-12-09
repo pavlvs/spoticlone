1495e40c7bd0230c38b6fca06271ce9d2c14bfde
Paul J Shaw <pavlvsxavier@gmail.com>
2019-12-09 12:49:08 +0000

    created the User class

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -1,9 +1,16 @@
 <?php
 if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])){
     include "includes/config.php";
+    include "includes/classes/User.php";
     include "includes/classes/Artist.php";
     include "includes/classes/Album.php";
     include "includes/classes/Song.php";
+
+    if(isset($_GET['userLoggedIn'])){
+        $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+    } else {
+        echo "username variable was not passed into the page. Check the openPage Js function";
+    }
 
 }else {
     include('includes/header.php');
