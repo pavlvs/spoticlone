7a17a601d1787438aa473a2b520d7158fe42fd2f
Paul J Shaw <pavlvsxavier@gmail.com>
2019-12-10 21:24:06 +0000

    displayed playlist songs

--- playlist.php
+++ playlist.php
@@ -12,62 +12,64 @@ $owner = new User($con, $playlist->getOwner());
 
 <div class="entityInfo">
 	<div class="leftSection">
-		<img src="assets/images/icons/playlist.png" alt="playlist image">
+		<div class="playlistImage">
+			<img src="assets/images/icons/playlist.png" alt="playlist image">
+		</div>
 	</div>
 	<div class="rightSection">
 		<h2><?php echo $playlist->getName(); ?></h2>
 		<p>By <?php echo $playlist->getOwner(); ?></p>
 		<p><?php echo $playlist->getNumberOfSongs(); ?> song(s)</p>
-		<button class="button">DELETE PLAYLIST</button>
+		<button class="button spoticlone">DELETE PLAYLIST</button>
 	</div>
 </div>
 
 <div class="tracklistContainer">
 	<ul class="tracklist">
 		<?php
-$songIdArray = array(); //$playlist->getSongIds();
+		$songIdArray = $playlist->getSongIds();
 
-$i = 1;
-foreach ($songIdArray as $songId) {
-	$playlistSong = new Song($con, $songId);
-	$playlistArtist = $playlistSong->getArtist();
-	echo <<<EOT
-<li class="tracklistRow">
-	<div class="trackCount">
-		<img class="play" src="assets/images/icons/play-white.png" onclick="setTrack('
-EOT;
-echo $playlistSong->getId();
+		$i = 1;
+		foreach ($songIdArray as $songId) {
+			$playlistSong = new Song($con, $songId);
+			$songArtist = $playlistSong->getArtist();
 echo <<<EOT
-', tempPlaylist, true)">
-		<span class="trackNumber">$i</span>
-	</div>
-	<div class="trackInfo">
-		<span class="trackName">
+		<li class="tracklistRow">
+			<div class="trackCount">
+				<img class="play" src="assets/images/icons/play-white.png" onclick="setTrack('
 EOT;
-	echo $playlistSong->getTitle();
-	echo <<<EOT
-		</span>
-		<span class="trackArtist">
+		echo $playlistSong->getId();
+echo <<<EOT
+		', tempPlaylist, true)">
+				<span class="trackNumber">$i</span>
+			</div>
+			<div class="trackInfo">
+				<span class="trackName">
 EOT;
-	echo $playlistArtist->getName();
-	echo <<<EOT
-		</span>
-	</div>
-	<div class="trackOptions">
-		<img class="optionsButton" src="assets/images/icons/more.png">
-	</div>
-	<div class="trackDuration">
-		<span class="duration">
+			echo $playlistSong->getTitle();
+echo <<<EOT
+				</span>
+				<span class="trackArtist">
 EOT;
-	echo $playlistSong->getDuration();
-	echo <<<EOT
-		</span>
-	</div>
-</li>
+			echo $songArtist->getName();
+echo <<<EOT
+				</span>
+			</div>
+			<div class="trackOptions">
+				<img class="optionsButton" src="assets/images/icons/more.png">
+			</div>
+			<div class="trackDuration">
+				<span class="duration">
 EOT;
-	$i++;
-}
-?>
+			echo $playlistSong->getDuration();
+echo <<<EOT
+				</span>
+			</div>
+		</li>
+EOT;
+			$i++;
+		}
+		?>
 <script >
 	var tempSongIds = '<?php echo json_encode($songIdArray);?>';
 	tempPlaylist = JSON.parse(tempSongIds);
