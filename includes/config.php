4a45881c08cf19807147736c23fe74361ec5619a
Paul Shaw <pavlvsxavier@gmail.com>
2020-08-13 13:51:06 +0100

    updated db connection details

--- includes/config.php
+++ includes/config.php
@@ -3,9 +3,8 @@ ob_start();
 session_start();
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("localhost", "root", "", "spoticlone");
+$con = mysqli_connect("localhost", "root", "", "slotify");
 
 if (mysqli_connect_errno()) {
 	echo "Failed to connect:" . mysqli_connect_errno();
 }
-?>
